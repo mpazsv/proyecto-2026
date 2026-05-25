be3a7edb9283ce19f189ecb34da1b569b2c95809
mpazsv <mpazsalazarv@gmail.com>
2026-05-24 22:09:43 -0400

    Add files via upload

---
+++ passport.auth.js
@@ -0,0 +1,46 @@
+"use strict"
+import passport from "passport";
+import{ Strategy as JwtStrategy, ExtractJwt } from "passport-jwt";
+import {AppDataSource} from "../config/configDb.js";
+import Usser from "../entity/user.entity.js";
+import { JWT_SECRET } from "../config/configEnv.js";
+
+const cookieExtractor = (req) => {
+    let token = null;
+    if (req && req.cookies){
+        token = req.cookies.jwt;
+    }
+    return token;
+};
+
+const option = {
+    jwtFromRequest: ExtractJwt.fromExtractors ([cookieExtractor]),
+    secretOrKey: JWT_SECRET,
+};
+
+export const passportJwtSetup = () => {
+    passport.use(
+        new JwtStrategy(option, async (payload, done) => {
+            try {
+                const userRepository = AppDataSource.getRepository(Usser);
+                const user = await userRepository.findOneBy({
+                    where : { id: payload.id },
+                });
+
+                if (user && user.estado_activo){
+                    return done(null, {
+                        id: user.id,
+                        email: user.email,
+                        rol: user.rol,
+                        nombre: user.nombre,
+                        apellido: user.apellido
+                    });
+                } else {
+                    return done(null, false);
+                }
+            } catch (error) {
+                return done(error, false);
+            }
+        })
+    );
+};
